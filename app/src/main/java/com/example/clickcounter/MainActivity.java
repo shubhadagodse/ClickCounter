46d69bca1064878018335d31b7040a5bbcb569bc
Shubhada Godse <shubhadagodse@gmail.com>
2019-04-18 13:21:57 +0530

    enhancements

--- app/src/main/java/com/example/clickcounter/MainActivity.java
+++ app/src/main/java/com/example/clickcounter/MainActivity.java
@@ -1,5 +1,4 @@
 package com.example.clickcounter;
-
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
@@ -7,72 +6,52 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Button;
-
 import static com.example.clickcounter.R.id.a_main_tv_btnCount;
-
-@SuppressWarnings("ALL")
 public class MainActivity extends AppCompatActivity {
-
     private int lastOrientation;
     public static final String KEY_LAST_ORIENTATION = "last_orientation";
-
     private TextView tvBtnCount;
     private TextView tvBgCount;
     private Button clickButton;
     private int btnCounter = 0;
     private int bgCounter = 0;
     protected static final String TAG = MainActivity.class.getName();
-
-    public static final String SHARED_PREFS = "shared preferences";
-    public static final String SHARED_PREFS_BTN_COUNTER= "BtnCounter";
-    public static final String SHARED_PREFS_BACKGROUND_COUNTER= "BgCounter";
-
+    private static final String SHARED_PREFS = "shared preferences";
+    private static final String SHARED_PREFS_BTN_COUNTER= "BtnCounter";
+    private static final String SHARED_PREFS_BACKGROUND_COUNTER= "BgCounter";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
-
         loadData();
-
         tvBtnCount = findViewById(a_main_tv_btnCount);
         tvBgCount = findViewById(R.id.a_main_tv_btnBgCount);
         clickButton = findViewById(R.id.a_main_btn1);
-
         tvBtnCount.setText("Button Count "+Integer.toString(btnCounter));
         tvBgCount.setText("Background Count "+Integer.toString(bgCounter));
-
         clickButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 btnCounter ++;
                 tvBtnCount.setText("Button Count "+Integer.toString(btnCounter));
                 saveData();
-
             }
         });
-
         if (savedInstanceState == null) {
             lastOrientation = getResources().getConfiguration().orientation;
         }
     }
-
     @Override
     protected void onResume() {
-
         tvBgCount.setText("Background Count " + bgCounter);
         super.onResume();
     }
-
     @Override
     protected void onStop() {
-
         if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT)
         {
             bgCounter++;
             tvBgCount.setText("Background Count " + bgCounter);
-
         }
         if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE)
         {
@@ -82,31 +61,27 @@ public class MainActivity extends AppCompatActivity {
         saveData();
         super.onStop();
     }
-
     @Override
     protected void onDestroy() {
         saveData();
         super.onDestroy();
     }
-
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putInt(SHARED_PREFS_BTN_COUNTER,btnCounter);
         outState.putInt(SHARED_PREFS_BACKGROUND_COUNTER,bgCounter);
     }
-
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
-        btnCounter =savedInstanceState.getInt(SHARED_PREFS_BTN_COUNTER);
+        btnCounter = savedInstanceState.getInt(SHARED_PREFS_BTN_COUNTER);
         bgCounter = savedInstanceState.getInt(SHARED_PREFS_BACKGROUND_COUNTER);
         tvBgCount = findViewById(R.id.a_main_tv_btnBgCount);
         tvBgCount.setText(Integer.toString(bgCounter));
     }
-
-    private void saveData(){
-        SharedPreferences sharedPreferences =getSharedPreferences(SHARED_PREFS,MODE_PRIVATE);
+    private void saveData() {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS,MODE_PRIVATE);
         SharedPreferences.Editor editor=sharedPreferences.edit();
 
         editor.putInt(SHARED_PREFS_BTN_COUNTER,btnCounter);
@@ -114,11 +89,9 @@ public class MainActivity extends AppCompatActivity {
         editor.apply();
         editor.commit();
     }
-
     private void loadData()
     {
-        SharedPreferences sharedPreferences=getSharedPreferences(SHARED_PREFS,MODE_PRIVATE);
-
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS,MODE_PRIVATE);
         btnCounter = sharedPreferences.getInt(SHARED_PREFS_BTN_COUNTER, 0);
         bgCounter = sharedPreferences.getInt(SHARED_PREFS_BACKGROUND_COUNTER,0);
     }
